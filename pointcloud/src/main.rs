1de47f2bfddb25c8a5b3d3e3197c30b0bd33fe77
igs <x7041@protonmail.com>
2022-01-12 12:41:12 +0100

    made pointcloud renderer compute unseen orbitals when finished rendering the currently visible one

--- pointcloud/src/main.rs
+++ pointcloud/src/main.rs
@@ -1,5 +1,4 @@
 use crate::molecules::{AMMONIA, BENZENE, METHANE, NITRITE, WATER};
-use basis_set::periodic_table::AtomType;
 use kiss3d::event::{Action, Key, WindowEvent};
 use kiss3d::text::Font;
 use kiss3d::window::Window;
@@ -17,13 +16,13 @@ struct DataPoint {
     prob: f32,
 }
 
+const POINTS_PER_N: usize = 1_000_000;
+const POINTS_PER_ITER: usize = 25_000;
+
 fn main() {
     let basis_set = &basis_set::basis_sets::BASIS_3_21G;
-    let molecule = [
-        basis_set.get(Vector3::zeros(), AtomType::Hydrogen),
-        basis_set.get(Vector3::new(bohr!(1.25), 0.0, 0.0), AtomType::Chlorine),
-    ];
-    if let Some(result) = molecule.try_scf(1000, 1e-6, 0) {
+    let molecule = BENZENE.build(basis_set);
+    if let Some(result) = molecule.try_scf(5000, 1e-6, 0) {
         let n_basis = result.orbitals.basis_functions().len();
 
         let mut rng = XorShiftRng::from_entropy();
@@ -41,29 +40,48 @@ fn main() {
         let mut min_prob = 0.02;
         while window.render() {
             window.set_title(&*format!(
-                "Energy Level: {}/{} (E: {:+0.3}eV) | total energy: {:+0.3} Hartrees",
+                "Energy Level: {}/{} (E: {:+0.2}eV) | total energy: {:+0.2} Hartrees",
                 n,
                 n_basis - 1,
                 result.orbital_energies[n] * 27.211, // <-- Hartree to eV conversion factor,
                 result.total_energy
             ));
-            if data_points[n].len() < 1_000_000 {
-                for _ in 0..50_000 {
-                    let point = (rng.gen::<Vector3<f64>>() - Vector3::repeat(0.5))
-                        .component_mul(&Vector3::new(7.5, 5.0, 7.5))
-                        * 2.0;
-                    let wave = result.orbitals.evaluate(point, n);
-                    let prob = wave.powi(2);
+            let curr_n = if data_points[n].len() < POINTS_PER_N {
+                Some(n)
+            } else {
+                (0..n_basis).find(|n| data_points[*n].len() < 1_000_000)
+            };
+            if let Some(n) = curr_n {
+                if data_points[n].len() < POINTS_PER_N {
+                    for _ in 0..POINTS_PER_ITER {
+                        let point = (rng.gen::<Vector3<f64>>() - Vector3::repeat(0.5))
+                            .component_mul(&Vector3::new(7.5, 5.0, 7.5))
+                            * 2.0;
+                        let wave = result.orbitals.evaluate(point, n);
+                        let prob = wave.powi(2);
 
-                    data_points[n].push(DataPoint {
-                        position: Point3::from(point.map(|f| f as f32)),
-                        color: if wave > 0.0 {
-                            Point3::new(1.0, 0.2, 0.2)
-                        } else {
-                            Point3::new(0.2, 0.2, 1.0)
-                        },
-                        prob: prob as f32,
-                    });
+                        data_points[n].push(DataPoint {
+                            position: Point3::from(point.map(|f| f as f32)),
+                            color: if wave > 0.0 {
+                                Point3::new(1.0, 0.2, 0.2)
+                            } else {
+                                Point3::new(0.2, 0.2, 1.0)
+                            },
+                            prob: prob as f32,
+                        });
+                    }
+
+                    window.draw_text(
+                        &*format!(
+                            "Currently filling energy level {}\n {} points",
+                            n,
+                            data_points[n].len()
+                        ),
+                        &Point2::new(10.0, 5.0),
+                        window.width() as f32 / 25.0,
+                        &Font::default(),
+                        &Point3::new(1.0, 1.0, 1.0),
+                    );
                 }
             }
 
@@ -83,14 +101,6 @@ fn main() {
                 .iter()
                 .filter(|point| point.prob > min_prob)
                 .for_each(|point| window.draw_point(&point.position, &point.color));
-
-            window.draw_text(
-                &*format!("{} points", data_points[n].len()),
-                &Point2::new(10.0, 5.0),
-                window.width() as f32 / 25.0,
-                &Font::default(),
-                &Point3::new(1.0, 1.0, 1.0),
-            );
         }
     } else {
         panic!("SCF didn't converge!");
